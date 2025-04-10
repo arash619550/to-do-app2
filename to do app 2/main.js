739e1be725a15b5f3af0cebb89a166da4afe1ec3
arash <arash619551@gmail.com>
2025-04-10 15:49:28 +0330

    Add files via upload

---
+++ to do app 2/main.js
@@ -0,0 +1,16 @@
+let tasks = [];
+let removeImg = document.getElementById("remove");
+let counter = 0;
+function addTask() {
+    const input = document.getElementById("input");
+    tasks.push(input.value);
+    removeImg.style.display = "inline-block";
+    removeImg.style.paddingRight = "10px";
+    if (input.value != "") {
+        document.body.append(tasks[counter], removeImg)
+        document.body.innerHTML += "<br><br>";
+    }
+    input.value = "";
+    counter++;
+    
+}
